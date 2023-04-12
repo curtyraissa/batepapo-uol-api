d86e100a2db68e14a272ad56eab14160f59d54a8
curtyraissa <raissacurty@gmail.com>
2023-04-11 22:25:52 -0300

    configuracoes dotenv e BD mongo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import dayjs from "dayjs";
+import dotenv from 'dotenv';
+dotenv.config();
+import { MongoClient } from "mongodb"; 
 
+const dayjs = require('dayjs');
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/participants", (request, response) => {
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+mongoClient.connect()
+.then(() => {db = mongoClient.db(); 
+}).catch((err) => console.log(err.message))
 
-  response.send("blablabla")
+app.post("/participants", (request, response) => {
+  const { name } = request.body
+  const novoParticipante = { name, lastStatus: Date.now() }
+  response.sendStatus(201);
 })
 
 app.get("/participants", (request, response) => {
