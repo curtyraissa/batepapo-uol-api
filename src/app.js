45b7c92e89a99bdcd5879406e0c9bfd0fea9e16a
curtyraissa <raissacurty@gmail.com>
2023-04-13 23:50:45 -0300

    utilizado validacao joy no post participants

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import cors from "cors";
 import dayjs from "dayjs";
 import dotenv from 'dotenv';
 dotenv.config();
-import { MongoClient } from "mongodb"; 
+import { MongoClient } from "mongodb";
+import joi from 'joi'
 
-const dayjs = require('dayjs');
+const dayjs = require(dayjs);
 const app = express();
+const Joi = require(joi);
 
 app.use(cors());
 app.use(express.json());
@@ -14,12 +16,19 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 mongoClient.connect()
-.then(() => {db = mongoClient.db(); 
-}).catch((err) => console.log(err.message))
+  .then(() => { db = mongoClient.db();
+  }).catch((err) => console.log(err.message))
 
 app.post("/participants", (request, response) => {
   const { name } = request.body
-  if (!name || name=="") {
+
+  const participantsSchema = joi.object({
+    name: joi.string().required(),
+  });
+
+  const validation = participantsSchema.validate(name, { abortEarly: false });
+
+  if (validation. error) {
     response.status(422).send("Todos os campos são obrigatórios!");
     return;
   }
@@ -27,31 +36,37 @@ app.post("/participants", (request, response) => {
   const nomeExiste = db.collection("participants").findOne({ name });
   if (nomeExiste) return res.sendStatus(409);
 
-  const promise = db.collection("participants").insertOne({name, lastStatus: Date.now()});
+  const promise = db.collection("participants").insertOne({ name: name, lastStatus: Date.now() });
   promise.then(() => res.sendStatus(201));
-  // promise.catch(() => res.sendStatus(500));
+  promise.catch(() => res.sendStatus(500));
 
-  const message = db.collection("messages").insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:mm:ss' });
+  const message = db.collection("messages").insertOne({ 
+    from: name, 
+    to: 'Todos', 
+    text: 'entra na sala...', 
+    type: 'status', 
+    time: 'HH:mm:ss' 
+  });
 })
 
 app.get("/participants", (request, response) => {
 
-  response.send("blablabla")
+  
 })
 
 app.post("/messages", (request, response) => {
 
-  response.send("blablabla")
+  
 })
 
 app.get("/messages", (request, response) => {
 
-  response.send("blablabla")
+  
 })
 
 app.post("/status", (request, response) => {
 
-  response.send("blablabla")
+  
 })
 
 const PORT = 5000;
