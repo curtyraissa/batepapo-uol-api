b75c541fe13d2a6754eb77bd9a6d636f1f7ca8aa
curtyraissa <raissacurty@gmail.com>
2023-04-15 01:42:25 -0300

    implementando async await post participants

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-// import dayjs from "dayjs";
+import dayjs from "dayjs";
 import dotenv from 'dotenv';
-dotenv.config();
 import joi from 'joi';
 
-// const dayjs = require("dayjs");
+const dayjs = require("dayjs");
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
@@ -17,79 +17,88 @@ mongoClient.connect()
   .then(() => db = mongoClient.db())
   .catch((err) => console.log(err.message))
 
-app.post("/participants", (req, res) => {
-  const { name } = req.body
+const participantsSchema = joi.object({
+  name: joi.string().required(),
+});
 
-  const participantsSchema = joi.object({
-    name: joi.string().required(),
-  });
+const messageScheme = joi.object({
+  from: joi.string().required(),
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required().valid("message", "private_message"),
+})
+
+app.post("/participants", async (req, res) => {
+  const { name } = req.body
 
   const validation = participantsSchema.validate(name, { abortEarly: false });
 
   if (validation.error) {
-    res.sendStatus(422);;
+    res.sendStatus(422);
     return;
   }
 
-  const nomeExiste = db.collection("participants").findOne({ name });
-  if (nomeExiste) return res.sendStatus(409);
+  try {
+    const nomeExiste = await db.collection("participants").findOne({ name });
+    if (nomeExiste) {
+      return res.sendStatus(409);
+    }
 
-  const promise = db.collection("participants").insertOne({ name: name, lastStatus: Date.now() });
-  promise.then(() => res.sendStatus(201));
-  promise.catch(() => res.sendStatus(500));
+    await db.collection("participants").insertOne({ 
+      name: name, 
+      lastStatus: Date.now() 
+    });
+    // .catch(() => res.sendStatus(500));
 
-  const mensagemInicial = db.collection("messages").insertOne({ 
-    from: name, 
-    to: 'Todos', 
-    text: 'entra na sala...', 
-    type: 'status', 
-    time: 'HH:mm:ss' 
-  });
-  mensagemInicial.then(() => res.sendStatus(201));
-  mensagemInicial.catch(() => res.sendStatus(500));
+    await db.collection("messages").insertOne({
+      from: name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs().format('HH:mm:ss')
+    });
+  } catch (err) {res.sendStatus(500)}
+
+  res.sendStatus(201);
 })
 
 app.get("/participants", (req, res) => {
-    const participants = db.collection("/participants").find().toArray()
-    if(!participants){
-      participants.catch((err) => res.status(404).send([]))
-    } else {
-      participants.then((participants) => res.status(201).send(participants))}     
+  const participants = db.collection("/participants").find().toArray()
+  if (!participants) {
+    participants.catch((err) => res.status(404).send([]))
+  } else {
+    participants.then((participants) => res.status(201).send(participants))
+  }
 })
 
 app.post("/messages", (req, res) => {
- const {to, text, type } = req.body;
- const { user } = req.headers;
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
 
-//  if(!user || !to || !text || !type)
-//   return res.sendStatus(422);
- 
-const messageScheme = joi.object ({
-  from: joi.string().required(),
-  to: joi.string().required(),
-  text: joi.string().required(),
-  type:joi.string().required().valid("message", "private_message"),
-})
+  //  if(!user || !to || !text || !type)
+  //   return res.sendStatus(422);
 
-const validation = messageScheme.validate(
-  {to, text, type, from: user}, {aboutEarly: false}
-)
 
-if (validation.error) {
-  res.sendStatus(422);
-  return;
-}
+  const validation = messageScheme.validate(
+    { to, text, type, from: user }, { aboutEarly: false }
+  )
+
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
 })
 
 // app.get("/messages", (req, resp) => {
 
-  
+
 // })
 
 // app.post("/status", (req, resp) => {
 
-  
+
 // })
 
+//rodando servidor na porta 5000
 const PORT = 5000;
 app.listen(PORT, () => { console.log(`servidor rodando na porta ${PORT}`) });
