1bf51f47d87ef7441aceebaf9694172179596977
curtyraissa <raissacurty@gmail.com>
2023-04-16 01:12:15 -0300

    criado post messages

--- src/app.js
+++ src/app.js
@@ -24,23 +24,23 @@ const messageScheme = joi.object({
   from: joi.string().required(),
   to: joi.string().required(),
   text: joi.string().required(),
-  type: joi.string().required().valid("message", "private_message"),
+  type: joi.string().valid("message", "private_message").required(),
 })
 
 app.post("/participants", async (req, res) => {
   const { name } = req.body
   const validation = participantsSchema.validate(name, { abortEarly: false });
 
-  // if (!validation) {
-  //   res.sendStatus(422);
-  //   return
-  // }
+  if (!validation) {
+    res.sendStatus(422);
+    return
+  }
 
-  if (validation.error) {
-		const errors = validation.error.details.map((type) => type.message);
-		res.status(422).send(errors);
-		return;
-	}
+  // if (validation.error) {
+  // 	const errors = validation.error.details.map((type) => type.message);
+  // 	res.status(422).send(errors);
+  // 	return;
+  // }
 
   try {
     const nomeExiste = await db.collection("participants").findOne({ name });
@@ -84,30 +84,46 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
   try {
     const { to, text, type } = req.body;
-    const { user } = req.headers;
+    const from = req.headers("User");
 
-    const formato = { from: user, to, text, type, time: dayjs().format("HH:mm:ss") }
-    //  if(!user || !to || !text || !type)
-    //   return res.sendStatus(422);
+    const { error } = messageScheme.validate({ to, text, type })
+    if (error) {
+      return res.status(422).send(error.details[0].message)
+    }
 
+    const existentes = await db.collection("messages").findOne({ name: from })
+    if (!existentes) {
+      return res.status(422).send("n existe")
+    }
 
-    const validation = messageScheme.validate(
-      formato, { aboutEarly: false }
-    )
+    const time = dayjs().format("HH:mm:ss")
+    const message = { from, to, text, type, time }
 
-    if (validation.error) {
-      const err = validation.error.details.map((e) => e.formato)
-      res.sendStatus(422).send(err);
-      return;
-    } const naoExiste = await db.collection("participants").insertOne({ name: participants })
-    if (!naoExiste) {
-      res.sendStatus(409)
-      return
-    } await db.collection("messages").findOne(formato);
-    res.sendStatus(201)
+    await db.collection("participants").insertOne(message)
+    return res.status(201).send()
+
   } catch (err) {
-    res.sendStatus(500);
+    return res.status(500).send(err.message)
   }
+
+
+  //     const formato = { from: user, to, text, type, time: dayjs().format("HH:mm:ss") }
+  //     const validation = messageScheme.validate(
+  //       formato, { aboutEarly: false }
+  //     )
+  //     if (validation.error) {
+  //       const err = validation.error.details.map((e) => e.formato)
+  //       res.sendStatus(422).send(err);
+  //       return;
+  //     } const naoExiste = await db.collection("participants").insertOne({ name: participants })
+  //     if (!naoExiste) {
+  //       res.sendStatus(409)
+  //       return
+  //     } await db.collection("messages").findOne(formato);
+  //     res.sendStatus(201)
+  //   } catch (err) {
+  //     res.sendStatus(500);
+  //   }
 })
 
 // app.get("/messages", (req, resp) => {
