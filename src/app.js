a07d5522f709fd156d8d0b5c9e79882edee24f7b
curtyraissa <raissacurty@gmail.com>
2023-04-17 14:33:41 -0300

    status 422

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const messageScheme = joi.object({
 
 app.post("/participants", async (req, res) => {
   const { name } = req.body
-  const validation = participantsSchema.validate(name, { abortEarly: false })
+  const {validation} = participantsSchema.validate(name, { abortEarly: false })
 
   if (validation) { // se for string ou vazio retorna 422
     res.sendStatus(422)
